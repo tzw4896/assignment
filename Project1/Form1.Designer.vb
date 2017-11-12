5127491305faebe3e32dbb1b280ee72866e05178
User <User@DESKTOP-C308M1J>
2017-11-13 01:02:17 +0800

    Add project files.

---
+++ Project1/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtICNum = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cbNationality = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblEmployee = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(103, 73)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(160, 171)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(83, 261)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(257, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 264)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Name : "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(5, 325)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Date of Birth :"
+        '
+        'txtICNum
+        '
+        Me.txtICNum.AutoSize = True
+        Me.txtICNum.Location = New System.Drawing.Point(26, 297)
+        Me.txtICNum.Name = "txtICNum"
+        Me.txtICNum.Size = New System.Drawing.Size(51, 13)
+        Me.txtICNum.TabIndex = 8
+        Me.txtICNum.Text = "IC Num : "
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(83, 294)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(154, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(83, 325)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 358)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Nationality : "
+        '
+        'cbNationality
+        '
+        Me.cbNationality.FormattingEnabled = True
+        Me.cbNationality.Items.AddRange(New Object() {"Choose One...", "Malaysia", "Foreign Citizen"})
+        Me.cbNationality.Location = New System.Drawing.Point(84, 358)
+        Me.cbNationality.Name = "cbNationality"
+        Me.cbNationality.Size = New System.Drawing.Size(121, 21)
+        Me.cbNationality.TabIndex = 14
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label4.Location = New System.Drawing.Point(31, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(156, 26)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Employee ID : "
+        '
+        'lblEmployee
+        '
+        Me.lblEmployee.AutoSize = True
+        Me.lblEmployee.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.lblEmployee.Location = New System.Drawing.Point(193, 26)
+        Me.lblEmployee.Name = "lblEmployee"
+        Me.lblEmployee.Size = New System.Drawing.Size(94, 26)
+        Me.lblEmployee.TabIndex = 16
+        Me.lblEmployee.Text = "DM1020"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(374, 702)
+        Me.Controls.Add(Me.lblEmployee)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cbNationality)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.txtICNum)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As Windows.Forms.PictureBox
+    Friend WithEvents txtName As Windows.Forms.TextBox
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents txtICNum As Windows.Forms.Label
+    Friend WithEvents TextBox3 As Windows.Forms.TextBox
+    Friend WithEvents DateTimePicker1 As Windows.Forms.DateTimePicker
+    Friend WithEvents Label3 As Windows.Forms.Label
+    Friend WithEvents cbNationality As Windows.Forms.ComboBox
+    Friend WithEvents Label4 As Windows.Forms.Label
+    Friend WithEvents lblEmployee As Windows.Forms.Label
+End Class
